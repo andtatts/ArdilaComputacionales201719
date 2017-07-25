13d4b67c87bebfafdd23f028cf1ecef07f5c5082
gustavoardila <ga.ardila10@uniandes.edu.co>
2017-07-25 16:51:14 -0500

    implementing MCMC

--- Homeworks/GustavoArdila_hw4/PuntoGeographicPoint.c
+++ Homeworks/GustavoArdila_hw4/PuntoGeographicPoint.c
@@ -6,8 +6,20 @@
 #define rows 500
 #define cols 744
 #define pi 3.1415926535
+#define iterations 380000
 //creates a pointer in order to create the matrix
 int **data;
+//test variables
+float tstx;
+float tsty;
+float tsta;
+//variables from the initial step
+float c_x;
+float c_y;
+float c_a;
+//M-H variabñes
+float alpha;
+float beta;
 //creates a function to read the file in order to make the main as simple as possible
 void initial(){
     int i;
@@ -22,7 +34,7 @@ void read_file(){
     const char *delim;
     delim=" ";
     char *split;
-    char line_buffer[800];
+    char line_buffer[1200];
     int i;
     int j;
     while(fgets(line_buffer, sizeof(line_buffer),file)){
@@ -63,8 +75,46 @@ double area(int x0, int y0){
     area=pi*pow(r,2);
     return area;
 }
+// generates random numbers from normal distribution, based on https://rosettacode.org/wiki/Statistics/Normal_distribution#C
+double random_normal(int mu){
+    double sigma=1.0;
+    double x1,x;
+    double y1,y;
+    double z;
+    double p;
+    x=2.0*rand()/RAND_MAX -1.0;
+    y=2.0*rand()/RAND_MAX -1.0;
+    z=pow(x,2)+pow(y,2);
+    while(z>=1.0||z==0.0){
+        p=sqrt(-2.0*log(z)/z);
+        x=p*x1;
+        y=p*y1;
+    }
+   double m=mu+sigma*x;
+    return m;
+}
+//tried implementing MH algorithm in a function but got confused so will implemeted in the main function
 int main(){
     initial();
     read_file();
+    /*
+    Metropolis Hastings:
+    Take an initial step within the box, dont include the 0th 
+    */
+    c_x=(int)(rows-2)*rand()/RAND_MAX;
+    c_y=(int)(cols-1)*rand()/RAND_MAX;
+    //calculate the area
+    c_a=area(c_x,c_y);
+    //iterates using metropolis hastings algorithm
+    int k;
+    //creates a counter to count the number of zeros in the circle
+    int count=0;
+    for(k=0;k<iterations;k++){
+        counter+=1;
+        tstx= (int)random_normal(c_x);
+        tsty= (int)random(c_y);
+        tsta= area(tstx,tsty);
+        
+    }
     return 0;
 }
