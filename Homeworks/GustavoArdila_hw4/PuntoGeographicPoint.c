710374e017968565e0ab46539b2ed3bdc30caedb
gustavoardila <ga.ardila10@uniandes.edu.co>
2017-07-25 14:06:01 -0500

    inital conditions and area

--- Homeworks/GustavoArdila_hw4/PuntoGeographicPoint.c
+++ Homeworks/GustavoArdila_hw4/PuntoGeographicPoint.c
@@ -5,9 +5,17 @@
 //number of rows and columns as global constants
 #define rows 500
 #define cols 744
+#define pi 3.1415926535
 //creates a pointer in order to create the matrix
-double **data;
+int **data;
 //creates a function to read the file in order to make the main as simple as possible
+void initial(){
+    int i;
+    data=malloc(rows*sizeof(int *));
+    for(i=0;i<rows;i++){
+        data[i]=malloc(cols*sizeof(int *));
+    }
+}
 void read_file(){
     FILE *file;
     file=fopen("map_data.txt","r");
@@ -27,6 +35,12 @@ void read_file(){
         j=0;
         i+=1;
     }
+    
+}
+//metropolis hastings to be implemented for the area
+double area(int x0, int y0){
+    double r=0;
+    area=pi*pow(r,2);
     
 }
 int main(){
