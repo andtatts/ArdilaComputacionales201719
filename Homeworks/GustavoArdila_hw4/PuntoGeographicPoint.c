bec1e290ee5226b1be2438d85af1f671d51fa7b7
gustavoardila <ga.ardila10@uniandes.edu.co>
2017-07-26 11:39:06 -0500

    while

--- Homeworks/GustavoArdila_hw4/PuntoGeographicPoint.c
+++ Homeworks/GustavoArdila_hw4/PuntoGeographicPoint.c
@@ -2,6 +2,7 @@
 #include<stdlib.h>
 #include<string.h>
 #include<math.h>
+#include<time.h>
 //number of rows and columns as global constants
 #define rows 500
 #define cols 744
@@ -9,6 +10,7 @@
 #define iterations 372000
 //creates a pointer in order to create the matrix
 int **data;
+//int *data[rows];
 //test variables
 /*
 float tstx;
@@ -34,21 +36,26 @@ void initial(){
     beta=0;
     c_a=0;
     int i;
-    data=malloc(rows*sizeof(int *));
-    for(i=0;i<rows;i++){
-        data[i]=malloc(cols*sizeof(int *));
+    data=malloc(cols*sizeof(int *));
+    for(i=0;i<cols;i++){
+        data[i]=malloc(rows*sizeof(int));
+        //data[i]=(int *)malloc(cols*sizeof(int));
     }
+    
 }
 void read_file(){
     FILE *file;
     file=fopen("map_data.txt","r");
+    if(file==NULL){
+        printf("paila\n");
+    }
     const char *delim;
     delim=" ";
     char *split;
-    char line_buffer[1200];
-    int i;
-    int j;
-    while(fgets(line_buffer, sizeof(line_buffer),file)){
+    char line_buffer[1800];
+    int i=0;
+    int j=0;
+    while(fgets(line_buffer, sizeof(line_buffer),file)!=NULL){
         split=strtok(line_buffer,delim);
         while(split!=NULL){
             data[i][j]=atof(split);
@@ -58,9 +65,10 @@ void read_file(){
         j=0;
         i+=1;
     }
-    
+    fclose(file);
 }
 //metropolis hastings to be implemented for the area
+
 double area(int x0, int y0){
     int x=x0;
     int y=y0;
@@ -71,10 +79,10 @@ double area(int x0, int y0){
     if(y+r>= cols-1){
          y=0;
      }  
-    if(y-r<0){
+    if(y-r<=0){
         y=cols-1;
     }
-    if(x-r<0){
+    if(x-r<=0){
         break;
     }
     if(x+r>=rows-1){
@@ -82,8 +90,11 @@ double area(int x0, int y0){
     }
     
     r+=1;
+    //printf("%d %d %d %d %d \n",x+r,x-r,y+r,y-r,r);
+        
     }
     area=pi*pow(r,2);
+    
     return area;
 }
 // generates random numbers from normal distribution, based on https://rosettacode.org/wiki/Statistics/Normal_distribution#C and https://phoxis.org/2013/05/04/generating-random-numbers-from-normal-distribution-in-c/
@@ -93,14 +104,17 @@ double random_normal(int mu){
     double y1,y;
     double z;
     double p;
-    x=2.0*rand()/RAND_MAX -1.0;
-    y=2.0*rand()/RAND_MAX -1.0;
+    x1=2.0*(double)rand()/RAND_MAX -1.0;
+    y1=2.0*(double)rand()/RAND_MAX -1.0;
     z=pow(x,2)+pow(y,2);
     while(z>=1.0||z==0.0){
-        p=sqrt(-2.0*log(z)/z);
-        x=p*x1;
-        y=p*y1;
+        x1=2.0*(double)rand()/RAND_MAX -1.0;
+        y1=2.0*(double)rand()/RAND_MAX -1.0;
+        z=pow(x,2)+pow(y,2);
+       
     }
+    p=sqrt(-2.0*log(z)/z);
+    x=x1*p;
    double m=mu+sigma*x;
     return m;
 }
@@ -108,19 +122,28 @@ double random_normal(int mu){
 int main(){
     initial();
     read_file();
+    
+    printf("wiiii\n");
+    
     /*
     Metropolis Hastings:
     Take an initial step within the box, dont include the 0th because it's the 0th col or row
     */
-    c_x=(int)(rows-1)*rand()/RAND_MAX;
-    c_y=(int)(cols-1)*rand()/RAND_MAX;
+    //SEEDS
+    srand(time(NULL));
+    c_x=(int)(rows-1)*(double)rand()/RAND_MAX;
+    c_y=(int)(cols-1)*(double)rand()/RAND_MAX;
+    printf("%d %d \n", c_x,c_y);
     //checks if the random numbers are different to 1
-    if(data[c_x][c_y]==1){
-        c_x=(int)(rows-2)*rand()/RAND_MAX;
-        c_y=(int)(cols-1)*rand()/RAND_MAX;
+    //if(data[c_x][c_y]==1){
+    while(data[c_x][c_y]==1){
+        c_x=(int)(rows-2)*(double)rand()/RAND_MAX;
+        c_y=(int)(cols-1)*(double)rand()/RAND_MAX;
+        printf("%d %d ",c_x,c_y);
     }
     //calculate the area
     c_a=area(c_x,c_y);
+    //printf("ola\n");
     //iterates using metropolis hastings algorithm
     int k;
     //creates a counter to count the number of zeros in the circle
@@ -129,14 +152,16 @@ int main(){
         count+=1;
         tstx= (int)random_normal(c_x);
         tsty= (int)random_normal(c_y);
-        //tsta= area(tstx,tsty);
+        tsta= area(tstx,tsty);
         //checks the condition of non producing a 1 or being out of bounds
-        if(data[tstx][tsty]==1||tstx+1>rows||tsty+1>cols|| tstx<0||tsty<0){
+        while(data[tstx][tsty]==1||tstx+1>rows||tsty+1>cols|| tstx<0||tsty<0){
              tstx= (int)random_normal(c_x);
-            tsty= (int)random_normal(c_y);
+             tsty= (int)random_normal(c_y);
+            printf("%d %d",tstx,tsty);
         }
         //gets the area of the test point
         tsta=area(tstx,tsty);
+        //printf("ola\n");
         //calculates the alpha from MH
         alpha=tsta/c_a;
         //checks the condition on alpha
@@ -148,9 +173,20 @@ int main(){
         //if not followed check beta
         else{
             //where is it centered?
-            beta=(int) random_normal()
+            beta=(double) rand()/RAND_MAX;
+            if(beta<alpha){
+                c_x=tstx;
+                c_y=tsty;
+                c_a=tsta;
+            }
+            else{
+                c_x=c_x;
+                c_y=c_y;
+                c_a=c_a;
+            }
         }
-        }
+    }
+    printf("%d %d \n",c_x,c_y);
     
     return 0;
 }
