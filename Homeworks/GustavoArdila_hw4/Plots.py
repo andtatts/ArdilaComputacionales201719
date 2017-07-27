ac3118f85042da1016d67c452055404f8abe4906
gustavoardila <ga.ardila10@uniandes.edu.co>
2017-07-27 10:35:15 -0500

    prints deleted

--- Homeworks/GustavoArdila_hw4/Plots.py
+++ Homeworks/GustavoArdila_hw4/Plots.py
@@ -1,34 +1,32 @@
-
-# coding: utf-8
-
-# In[17]:
-
 import numpy as np
 import matplotlib.pyplot as plt
 
-
-# In[22]:
-
+#imports the data and creates a  numpy array
 mapamundi=np.genfromtxt("map_data.txt")
+#imports the data generated from the c script and creates an array
 data=np.loadtxt('coordinates.txt')
+#from data creates the corresponding coordinates
 coordx=data[0]
 coordy=data[1]
 radio=data[2]
+
 #mapamundi=np.loadtxt('map_data.txt')
 xy=(coordx,coordy)
-print data
+#print data
 
 
-# In[23]:
-
+#plots
 plt.figure(1)
+#as a suggestion from Alfredo Ricci, the map has been plotted using an imshow
 plt.imshow(mapamundi)
+#scatter to plot the point
 plt.scatter(coordx,coordy)
+#searching in the matplotlib documentation, the function circle creates the circle we need
 plt.Circle(xy, radius=radio)
 plt.savefig('PuntoNemo.pdf')
 
 
-# In[ ]:
+
 
 
 
