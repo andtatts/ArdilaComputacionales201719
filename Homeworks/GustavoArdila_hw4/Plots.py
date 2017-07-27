422a24261424fae2191a289b8432007235e3cc66
gustavoardila <ga.ardila10@uniandes.edu.co>
2017-07-26 19:25:05 -0500

    works

--- Homeworks/GustavoArdila_hw4/Plots.py
+++ Homeworks/GustavoArdila_hw4/Plots.py
@@ -1,2 +1,34 @@
+
+# coding: utf-8
+
+# In[17]:
+
 import numpy as np
 import matplotlib.pyplot as plt
+
+
+# In[22]:
+
+mapamundi=np.genfromtxt("map_data.txt")
+data=np.loadtxt('coordinates.txt')
+coordx=data[0]
+coordy=data[1]
+radio=data[2]
+#mapamundi=np.loadtxt('map_data.txt')
+xy=(coordx,coordy)
+print data
+
+
+# In[23]:
+
+plt.figure(1)
+plt.imshow(mapamundi)
+plt.scatter(coordx,coordy)
+plt.Circle(xy, radius=radio)
+plt.savefig('PuntoNemo.pdf')
+
+
+# In[ ]:
+
+
+
