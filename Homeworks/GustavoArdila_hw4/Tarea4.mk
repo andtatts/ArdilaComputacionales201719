cbabd123b34b072e3927196df589a2b2f2aaea8e
gustavoardila <ga.ardila10@uniandes.edu.co>
2017-07-26 19:32:56 -0500

    does not work

--- Homeworks/GustavoArdila_hw4/Tarea4.mk
+++ Homeworks/GustavoArdila_hw4/Tarea4.mk
@@ -1,6 +1,6 @@
-PuntoNemo.pdf: Plots.py
-    python -lm Plots.py
-Plots.py: out.x
+PuntoNemo.pdf : Plots.py
+    python  Plots.py
+Plots.py : out.x
     ./out.x
-out.x: PuntoGeographicPoint.c
-    cc PuntoGeographicPoint.c -o out.x
+out.x : PuntoGeographicPoint.c
+    cc PuntoGeographicPoint.c -lm -o out.x
